971a9f6f22c26997106133cd2cea8f4553fc71f7
moizlok <moizlokhandwala09@gmail.com>
2023-01-19 20:01:32 +0530

    added clerk.dev

--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,81 @@
-import Router from 'next/router';
-import Head from 'next/head';
-import NProgress from 'nprogress';
-import { ChakraProvider } from '@chakra-ui/react';
+import Router, { useRouter } from "next/router";
+import Head from "next/head";
+import NProgress from "nprogress";
+import { ChakraProvider } from "@chakra-ui/react";
+import {
+  ClerkProvider,
+  RedirectToSignIn,
+  SignedIn,
+  SignedOut,
+} from "@clerk/nextjs";
 
-import Layout from '../components/Layout';
+import Layout from "../components/Layout";
 
-export default function App({ Component, pageProps }) {
+
+
+const publicPages = ["/sign-in/[[...index]]", "/sign-up/[[...index]]"];
+
+ function App({ Component, pageProps }) {
+  const clerkFrontendApi = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
+  const router = useRouter();
   NProgress.configure({ showSpinner: false });
 
-  Router.events.on('routeChangeStart', () => {
+  Router.events.on("routeChangeStart", () => {
     NProgress.start();
   });
 
-  Router.events.on('routeChangeComplete', () => {
+  Router.events.on("routeChangeComplete", () => {
     NProgress.done();
   });
 
-
   return (
-    <>
-       <Head>
-        <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css' integrity='sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ==' crossOrigin='anonymous' referrerPolicy='no-referrer' />
-      </Head>
-      <ChakraProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ChakraProvider>
-    </>
-  )
+    <ClerkProvider
+      frontendApi= {clerkFrontendApi}
+      navigate={(to) => router.push(to)}
+    >
+      {publicPages.includes(router.pathname) ? (
+        <>
+          <Head>
+            <link
+              rel="stylesheet"
+              href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
+              integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=="
+              crossOrigin="anonymous"
+              referrerPolicy="no-referrer"
+            />
+          </Head>
+          <ChakraProvider>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </ChakraProvider>
+        </>
+      ) : (
+        <>
+          <SignedIn>
+            <Head>
+              <link
+                rel="stylesheet"
+                href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
+                integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=="
+                crossOrigin="anonymous"
+                referrerPolicy="no-referrer"
+              />
+            </Head>
+            <ChakraProvider>
+              <Layout>
+                <Component {...pageProps} />
+              </Layout>
+            </ChakraProvider>
+          </SignedIn>
+
+          <SignedOut>
+            <RedirectToSignIn />
+          </SignedOut>
+        </>
+      )}
+    </ClerkProvider>
+  );
 }
+
+export default App;
